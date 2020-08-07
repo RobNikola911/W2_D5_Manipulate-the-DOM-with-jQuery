4ead1f71015888105b184d1b99986730df4e4cae
RobNikola911 <pajic1991@gmail.com>
2020-08-07 16:41:30 +0200

    updated

--- scripts.js
+++ scripts.js
@@ -1,25 +1,19 @@
-let $names = ["Steve ", "Robert ", "William ", "Kenia "];
+let names = ["Steve ", "Robert ", "William ", "Kenia "];
+names.forEach(element => {
+    if (element === "Robert ") {
+        $("ul").append('<li><strong>' + element + '</strong></li>');
+    }
+    else {
+        $("ul").append('<li>' + element + '</li>');
+    }
+});
 
-// $names.forEach("ul");
 
-$("ul").append(
-    "<li></li>" + $names
-);
-
-
-
-/* var $elements = [];
-for (var i = 0; i < 10; i++) {
-    $elements.push($('<div>', { text: 'Element ' + i}));
-}
-
-$('#some-element').append($elements); */
-
-let $additionalBlock = {
-    $title: "Added with javascript",
-    $text: "This block was added using JavaScript's jQuery library. How awesome!"
+let additionalBlock = {
+    title: "Added with javascript",
+    text: "This block was added using JavaScript's jQuery library. How awesome!"
   };
+  console.log(additionalBlock.text)
 
-  $("h1").append(
-      $additionalBlock
-  )
+
+    $("body").append("<header>" + additionalBlock.title + "</header>" + "<p>" + additionalBlock.text + "</p>")
